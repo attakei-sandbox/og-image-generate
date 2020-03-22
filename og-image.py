709cc4265327d0dea2b9b41c34de0b067032748a
Kazuya Takei <myself@attakei.net>
2020-03-22 23:40:54 +0900

    Write title text (stub)

--- og-image.py
+++ og-image.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import argparse
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 
 IMAGE_SIZE = (1200, 630)
 IMAGE_BG = (255, 255, 255, 255)
 LOGO_POS = (65, 65)
 LOGO_SIZE = (500, 500)
+TEXT_POS = (600, 65)
+TEXT_COLOR = (0, 0, 0, 255)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("logo", help="Logo image file path")
@@ -22,6 +24,10 @@ def main(args: argparse.Namespace):
     # Past logo image
     logo = Image.open(args.logo).resize(LOGO_SIZE)
     image.paste(logo, LOGO_POS)
+    # Write text
+    font = ImageFont.truetype("./mplus-1m-regular.ttf", size=64)
+    text_draw = ImageDraw.Draw(image)
+    text_draw.text(TEXT_POS, "hello world", font=font, fill=TEXT_COLOR)
     # Saving image
     image.save(args.out)
 
